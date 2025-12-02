f0bcc2c4b8db91e49f2362eeacb8edf88ed3b831
Elda Bello <elda822@gmail.com>
2025-12-02 15:28:54 -0400

    Fix en css de utilidad

--- src/utils/statusColor.js
+++ src/utils/statusColor.js
@@ -1,5 +1,6 @@
 export const getStatusColor = (status) => {
+    const baseClasses = 'px-3 py-1 rounded-full text-xs font-semibold';
   return status === 'Activo'
-    ? 'px-3 py-1 rounded-full text-xs font-semibold bg-green-100 text-green-800 hover:border-green-600 hover:border-2'
-    : 'px-3 py-1 rounded-full text-xs font-semibold bg-red-100 text-red-800 hover:border-red-600 hover:border';
+    ? `${baseClasses} bg-green-100 text-green-800 hover:border-green-600 hover:border-2`
+    : `${baseClasses} bg-red-100 text-red-800 hover:border-red-600 hover:border`;
 };
