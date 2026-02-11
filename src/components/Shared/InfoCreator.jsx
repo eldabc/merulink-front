0c343c60f097eb3bf82ca21df86788ced574a5a8
Elda Bello <elda822@gmail.com>
2026-02-11 09:50:20 -0400

    Cambio para mostrar input al crear plantilla

--- src/components/Shared/InfoCreator.jsx
+++ src/components/Shared/InfoCreator.jsx
@@ -1,6 +1,6 @@
 function InfoCreator({createdBy}) {
 return (
-    <div className="ml-auto text-sm text-gray-200 italic">
+    <div className=" text-sm text-gray-200 italic">
       Evento creado por: <span className="font-medium text-[#9fd8ff]">{createdBy}</span>
     </div>
   );
--- src/components/Shared/ToggleCreateTemplate.jsx
+++ src/components/Shared/ToggleCreateTemplate.jsx
@@ -7,13 +7,16 @@ function ToggleCreateTemplate({readOnly}) {
     setIsTemplate(!isTemplate);
   }
   return (
-    <div className="flex items-center gap-4">
-      <span className="text-sm font-medium  mr-2 text-gray-200">Crear Plantilla</span>
+    <div className="flex items-center gap-4 w-full max-w-2xl py-2">
+      <span className="text-sm font-medium text-gray-200 whitespace-nowrap">
+        Crear Plantilla
+      </span>
+      
       <div
         onClick={() => !readOnly && toggleIsTemplate()}
-        className={`relative inline-flex h-6 w-11 items-center rounded-full transition-colors duration-200 focus:outline-none ${
+        className={`relative inline-flex h-6 w-11 shrink-0 items-center rounded-full transition-colors duration-200 focus:outline-none ${
           isTemplate ? 'bg-[#9fd8ff]' : 'bg-gray-300'
-        }`}
+        } ${readOnly ? 'opacity-50 cursor-not-allowed pointer-events-none' : 'cursor-pointer'}`}
       >
         <span
           className={`inline-block h-4 w-4 transform rounded-full bg-white transition duration-200 ${
@@ -21,6 +24,13 @@ function ToggleCreateTemplate({readOnly}) {
           }`}
         />
       </div>
+      {isTemplate && (
+        <input 
+          type="text" 
+          placeholder="Nombre de la plantilla..."
+          className="w-full px-3 py-2 rounded-lg filter-input border"
+        />
+      )}
     </div>
   );
 }
