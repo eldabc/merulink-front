b11ec7aa889f4bd7b6699aa45a80c8a690bdd0c1
Elda Bello <elda822@gmail.com>
2026-05-06 11:26:21 -0400

    Uso de TabButtons en form de empleados.

--- src/components/Employee/tabs/TabButtonsManager.jsx
+++ src/components/Employee/tabs/TabButtonsManager.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { tabs } from '../../../utils/tabs-utils';
+import TabButtons from '../../Shared/TabButtons';
 
 export default function TabButtonsManager({ activeTab, setActiveTab, errors }) {
     return (
@@ -22,20 +23,28 @@ export default function TabButtonsManager({ activeTab, setActiveTab, errors }) {
             })();
 
             return (
-              <div key={tab.id} className="flex flex-col items-center sm:items-center"> 
-                <button
-                  type='button'
-                  onClick={() => setActiveTab(tab.id)}
-                  className={`px-4 py-2 border-b-2 transition-all text-xl font-bold text-white-700 mt-6 mb-2 p-2
-                    ${activeTab === tab.id
-                      ? "border-blue-500 text-[#9fd8ff]"
-                      : "border-transparent text-gray-400 hover:text-gray-200"}
-                  `}
-                >
-                  {tab.label}
-                  {tabError && ( <p className="px-2 py-1 rounded-full text-xs font-semibold bg-red-255 text-red-400 hover:text-red-800">Tienes campos erróneos en esta pestaña</p> )}
-                </button>
-              </div>
+              <TabButtons 
+                key={tab.id} 
+                tabId={tab.id} 
+                setActiveTab={setActiveTab} 
+                activeTab={activeTab} 
+                tabLabel={tab.label} 
+                tabError={tabError} 
+              />
+              // <div key={tab.id} className="flex flex-col items-center sm:items-center"> 
+              //   <button
+              //     type='button'
+              //     onClick={() => setActiveTab(tab.id)}
+              //     className={`px-4 py-2 border-b-2 transition-all text-xl font-bold text-white-700 mt-6 mb-2 p-2
+              //       ${activeTab === tab.id
+              //         ? "border-blue-500 text-[#9fd8ff]"
+              //         : "border-transparent text-gray-400 hover:text-gray-200"}
+              //     `}
+              //   >
+              //     {tab.label}
+              //     {tabError && ( <p className="px-2 py-1 rounded-full text-xs font-semibold bg-red-255 text-red-400 hover:text-red-800">Tienes campos erróneos en esta pestaña</p> )}
+              //   </button>
+              // </div>
             );
 
           })}
