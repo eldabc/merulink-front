194cf106e3db9d035422a5909456dc2cef97affb
Elda Bello <elda822@gmail.com>
2026-08-19 09:14:38 -0400

    Fixes en Badge de empleados.

--- src/components/Employee/EmployeeRow.jsx
+++ src/components/Employee/EmployeeRow.jsx
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs';
 import { useNavigate } from 'react-router-dom';
 import { useEmployees } from '../../context/EmployeeContext';
 
@@ -8,13 +9,20 @@ import ChangeStatusModal from './modals/ChangeStatusModal';
 import useChangeStatusModal from '../../hooks/useChangeStatusModal';
 import SpanText from '../Shared/SpanText';
 import HasPermission from '../Shared/HasPermission';
+import AlertBadge from '../Shared/AlertBadge';
+
 
 export default function EmployeeRow({ emp }) {
 
   const navigate = useNavigate();
   const { loadingChangeStatus } = useEmployees();
   const { isModalOpen, openChangeStatus, confirmChangeStatus, closeChangeStatus } = useChangeStatusModal();
-  
+  const hasScheduledDeactivate = emp?.latestPeriod?.scheduledDeactivateDate ? { 
+    type: 'warning', 
+    tooltip: 'Baja Programada para '+dayjs(emp?.latestPeriod?.scheduledDeactivateDate).format('DD/MM/YYYY'),
+    label: 'BAJA'
+  } : null;
+
   const handleSelectedEmployee = (id) => {
     navigate(`/empleados/ver/${id}`, { 
       state: { data: emp } 
@@ -40,17 +48,20 @@ export default function EmployeeRow({ emp }) {
       <td className="px-4 py-3 text-white-700">{subDepartmentName}</td>
       <td className="px-4 py-3 text-white-700">{emp.position.name}</td>
       <td className="px-4 py-3">
-        <HasPermission permissions={["change-status-employees"]} fallback={<SpanText text={getStatusName(emp.status)} />}>
-          <span 
-            className={getStatusColor(emp.status)}
-            onClick={(e) => {
-              e.stopPropagation();
-              openChangeStatus(emp);
-            }}
-          >
-            {getStatusName(emp.status)}
-          </span>
-        </HasPermission>
+        <span className="relative inline-block">
+          {hasScheduledDeactivate && <AlertBadge alert={hasScheduledDeactivate} dynamicClasses="-top-1" /> }
+          <HasPermission permissions={["change-status-employees"]} fallback={<SpanText text={getStatusName(emp.status)} />}>
+            <span 
+              className={getStatusColor(emp.status)}
+              onClick={(e) => {
+                e.stopPropagation();
+                openChangeStatus(emp);
+              }}
+            >
+              {getStatusName(emp.status)}
+            </span>
+          </HasPermission>
+        </span>
       </td>
     </tr>
     <tr>
