ab55f4d902cf3b0166b53d519440423675791e81
Elda Bello <elda822@gmail.com>
2026-08-21 08:51:13 -0400

    Loader en filas de listados.

--- src/components/Shared/RowTableLoading.jsx
+++ src/components/Shared/RowTableLoading.jsx
@@ -1,8 +1,10 @@
+import LoadingSpinner from './LoadingSpinner';
+
 function RowTableLoading ({colSpan = 4}) {
   return (
     <tr className="w-full text-center bg-gray-600">
       <td colSpan={colSpan} className="justify-center mt-2 text-[14px] text-gray-50 text-shadow-amber-50 p-2">
-        Cargando datos desde API...
+        <LoadingSpinner className="py-0" />
       </td>
     </tr>
   );
